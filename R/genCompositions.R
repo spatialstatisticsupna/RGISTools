6e4295005f62de2725e3a4fc94b72a1df61dd4a3
Unai <unai@hamahiru.org>
2019-06-14 12:19:09 +0200

    Changes in the run mode of genComposition

--- R/genCompositions.R
+++ R/genCompositions.R
@@ -13,10 +13,10 @@
 #'
 #' @param rstack a \code{RasterStack} where every layer is named after the 
 #' date it was captured in julian format as "\code{YYYYJJJ}".
-#' @param n.days number of days considered in the temporal window.
+#' @param n number of images combined in the aggregation.
 #' @param fun the function used to create the composite, such as \code{max}, \code{min}, \code{mean}, ...
-#' @param by.periods logical argument. If \code{TRUE} the function will aggregate.
-#' \code{n.days} periods instead of use \code{n.days} temporal window.
+#' @param by.days logical argument. If \code{FALSE}, \code{n} indicates the number of images.
+#' If \code{TRUE}, \code{n} indicates the number of days. 
 #' @param ... argument to allow function nestering.
 #' \itemize{
 #'   \item \code{AppRoot} the path where the images will be saved in tif image format.
@@ -27,40 +27,40 @@
 #' data("ex.ndvi.navarre")
 #' # show the images: clouds are found
 #' genPlotGIS(ex.ndvi.navarre)
-#' # Composites dividing the series in two periods and using MVC
+#' # Equivalent: Use 3 images for every composite
 #' composite.NDVI.1 <- genCompositions(ex.ndvi.navarre,
-#'                                     n.days = 2,
-#'                                     by.periods = TRUE,
+#'                                     n = 3,
+#'                                     fun = max)
+#' genPlotGIS(composite.NDVI.1)
+#' # Composites dividing the series in two periods and using MVC
+#' composite.NDVI.2 <- genCompositions(ex.ndvi.navarre,
+#'                                     n = 2,
+#'                                     by.days = TRUE,
 #'                                     fun = max)
 #' # Check that the clouds were removed
-#' genPlotGIS(composite.NDVI.1)
-#' # Equivalent: Use 3 images for every composite
-#' composite.NDVI.2 <- genCompositions(ex.ndvi.navarre,
-#'                                     n.days = 3,
-#'                                     fun = max)
 #' genPlotGIS(composite.NDVI.2)
-genCompositions<-function(rstack,n.days,fun,by.periods=FALSE,...){
+genCompositions<-function(rstack,n,fun,by.days=FALSE,...){
   args<-list(...)
   AppRoot<-defineAppRoot(...)
-  if(!by.periods){
+  if(by.days){
     # Create compositions using dates
     dates<-genGetDates(names(rstack))
     mxdate<-max(dates)
     mndate<-min(dates)
     alldates<-as.Date(mndate:mxdate)
     n.ped<-length(alldates)
-    rnp<-ceiling(n.ped/n.days)
-    idx<-rep(1:rnp,each=n.days)[1:length(alldates)]
+    rnp<-ceiling(n.ped/n)
+    idx<-rep(1:rnp,each=n)[1:length(alldates)]
     idx<-idx[alldates%in%dates]
     comp<-stackApply(rstack,idx,fun=fun, ...)
-    names(comp)<-paste0("Comp_",n.days,"_",format(alldates,"%Y%j")[unique(idx)])
+    names(comp)<-paste0("Comp_",n,"_",format(alldates,"%Y%j")[unique(idx)])
   }else{
     # Create compositions using periods
     n.ped<-nlayers(rstack)
-    rnp<-ceiling(n.ped/n.days)
-    idx<-rep(1:rnp,each=n.days)[1:n.ped]
+    rnp<-ceiling(n.ped/n)
+    idx<-rep(1:rnp,each=n)[1:n.ped]
     comp<-stackApply(rstack,idx,fun=fun, ...)
-    names(comp)<-paste0(names(rstack)[unique(idx)],"_Comp_",n.days)
+    names(comp)<-paste0(names(rstack)[unique(idx)],"_Comp_",n)
   }
   if("AppRoot"%in%names(args)){
     writeRaster(comp,filename=paste0(AppRoot,"/",names(comp),".tif"),bylayer=T)
