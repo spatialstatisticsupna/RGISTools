15ce261f8458b743457757a0657acb33772a903e
unai-perez <unai.perez@unavarra.es>
2019-10-11 11:11:33 +0200

    file name change

--- R/lsDownSearch.R
+++ R/lsDownSearch.R
@@ -1,40 +1,49 @@
-#' Download Landsat-7 or Landsat-8 images from a search list
+#' Search and download Landsat-7 or Landsat-8 images
 #'
-#' \code{lsDownload} downloads the results from \code{\link{ls7Search}} and
-#' \code{\link{ls8Search}} functions. The images are saved as GTiff files in the
-#' \code{AppRoot} directory.
+#' \code{lsDownSearch} searches and downloads Landsat-7 or Landsat-8 images
+#' concerning a particular location and time interval from the 
+#' \href{https://earthexplorer.usgs.gov/}{`EarthExplorer' repository}.
+#' Images are saved as GTiff files in the \code{AppRoot} directory.
 #'
-#' \code{lsDonwSearch} downloads the list of URLs provided by \link{ls7Search}
-#' or \link{ls8Search} as a \code{data.frame}. The function requires an USGS's
-#' `EarthExplorer' account, which can be obtained 
+#' \code{lsDownSearch} is a wrapper function of \code{\link{ls7Search}}, 
+#' \code{\link{ls8Search}}, and \code{\link{lsDownload}} to search and
+#' download images in a single step. The function requires USGS's `EarthExplorer'
+#' credentials, which can be obtained
 #' \href{https://ers.cr.usgs.gov/register/}{here}.
 #' 
-#' The files from `EarthExplorer' are compressed as ‘tar.gz’. \code{lsDownload}
-#' decompresses the images and obtains the corresponding GTiffs. The GTiff files
-#' are saved in the \code{AppRoot} directory. To change this option, provide
-#' \code{AppRoot = “full path”}. When \code{untar = TRUE}, the function
-#' decompresses the imagery. Image decompression duplicates the information
-#' due to the presence of both, compressed and decompressed images. Set 
-#' \code{raw.rm = TRUE} to remove former ones.
+#' The files from `EarthExplorer' are compressed as ‘tar.gz’. \code{lsDownSearch}
+#' decompresses the images and obtains the corresponding GTiffs. The GTiffs are
+#' saved in the \code{AppRoot} directory. To change this option, provide 
+#' \code{AppRoot = “full path”}. When the \code{untarDir} argument is defined,
+#' the function untars the imagery in this location. Image decompression
+#' duplicates the information due to the presence of both, compressed and 
+#' decompressed images. Set \code{raw.rm = TRUE} to remove the former ones.
 #' 
-#' @param searchres the results from \code{\link{ls7Search}} or 
-#' \code{\link{ls8Search}}.
-#' @param username USGS's `EarthExplorer' username.
-#' @param password USGS's `EarthExplorer' password.
+#' @param satellite string containing the type of satellite 
+#' (\code{"ls7"} or \code{"ls8"}).
+#' @param startDate a \code{Date} class object with the starting date of the 
+#' study period.
+#' @param endDate a \code{Date} class object with the ending date of the 
+#' study period.
 #' @param AppRoot the download directory.
+#' @param username USGS’s `EarthExplorer' username.
+#' @param password USGS’s `EarthExplorer' password.
 #' @param lvl a number specifying the processing level. Default value, 1.
 #' @param product \code{character} vector with the requested Level-2 products.
-#' By default \code{c("sr","source_metadata")}.
-#' @param verbose logical argument. If \code{TRUE}, the function prints the 
+#' By default \code{c("sr", "source_metadata")}.
+#' @param verbose logical argument. If \code{TRUE}, the function prints the
 #' running steps and warnings.
 #' @param raw.rm logical argument. If \code{TRUE}, removes the raw images.
 #' @param untar logical argument. If \code{TRUE}, untars downloaded images.
-#' @param nattempts the number of attempts to download an image in case it
-#' becomes corrupted.
-#' @param overwrite logical argument. If \code{TRUE}, overwrites the existing
-#' images with the same name.
-#' @param ... arguments for nested functions.
+#' @param ... argumetns for nested functions:
+#'  \itemize{
+#'   \item any argument for \code{\link{ls8Search}}/\code{\link{ls7Search}} or 
+#'   \code{\link{lsDownload}}.
+#' }
 #'
+#' @return this function does not return anything. It saves the imagery as
+#' `tar.gz’ (and GTiff files) in a folder called `raw’ (`untar’) in the
+#'  \code{AppRoot} directory.
 #'
 #' @examples
 #' \dontrun{
@@ -43,153 +52,59 @@
 #' 
 #' src <- paste0(tempdir(),"/Path_for_downloading_folder")
 #' print(src)
-#' 
 #' # search and download the images from Landsat-8 between
-#' # 2011 and 2013 in the region of Navarre
-#' search.res <- ls8Search(startDate = as.Date("01-01-2018", "%d-%m-%Y"),
-#'                         endDate = as.Date("20-01-2018", "%d-%m-%Y"),
-#'                         extent = ex.navarre,
-#'                         browseAvaliable = "Y",
-#'                         AppRoot = src)
-#'
-#' # download 1 image
-#' lsDownload(searchres = search.res[1,], 
-#'            username = "user", 
-#'            password = "pass", 
-#'            AppRoot = src,
-#'            untar = TRUE)
-#' # download 4 images
-#' lsDownload(searchres = search.res[1:4,], 
-#'            username = "user", 
-#'            password = "pass",
-#'            AppRoot = src, 
-#'            untar = TRUE)
-#' # download all the images
-#' lsDownload(searchres = search.res, 
-#'            username = "user", 
-#'            password = "pass",
-#'            AppRoot = src,
-#'            untar = TRUE)
-#'
-#' # search and download the images from Landsat-7 between
-#' # 2011 and 2013 in the region of Navarre
-#' src <- paste0(tempdir(),"/Path_for_downloading_folder")
-#' print(src)
-#' search.res <- ls7Search(startDate = as.Date("01-01-2018", "%d-%m-%Y"),
-#'                         endDate = as.Date("20-01-2018", "%d-%m-%Y"),
-#'                         extent = ex.navarre,
-#'                         browseAvaliable = "Y",
-#'                         AppRoot = src)
-#' # download 1 image
-#' lsDownload(searchres = search.res[1,], 
-#'            username = "user", 
-#'            password = "pass", 
-#'            untar = TRUE,
-#'            AppRoot = src)
-#' # download 4 images
-#' lsDownload(searchres = search.res[1:4,], 
-#'            username = "user", 
-#'            password = "pass", 
-#'            untar = TRUE, 
-#'            AppRoot = src)
-#' # download all the images
-#' lsDownload(searchres = search.res, 
-#'            username = "user", 
-#'            password = "pass", 
-#'            untar = TRUE, 
-#'            AppRoot = src)
-#' 
-#' # removes the metadata to free memory space
+#' # 01-01-2018 and 20-01-2018 for the region of Navarre
+#' lsDownSearch(satellite = "ls8",
+#'              username = "username",
+#'              password = "password",
+#'              startDate = as.Date("01-01-2018", "%d-%m-%Y"),
+#'              endDate = as.Date("20-01-2018", "%d-%m-%Y"),
+#'              extent = ex.navarre,
+#'              AppRoot = src)
+#'            
+#' # remove metadata to free memory space
 #' lsRemoveMetadata()
-#' 
-#' # select Landsat-7 RGB bands
-#' src.ls7 <- file.path(src,"Landsat7")
-#' files <- list.files(src.ls7, 
-#'                     pattern = "\\.TIF$", 
-#'                     full.names = TRUE, 
-#'                     recursive = TRUE)[c(6,5,4)]
-#' files.stack <- stack(files)
-#' qrange <- c(0.001, 0.999)
-#' imagen <- varRGB(files.stack[[1]], 
-#'                  files.stack[[2]],
-#'                  files.stack[[3]],
-#'                  qrange)
-#' plotRGB(imagen)
 #' }
-lsDownload<-function(searchres,
-                     username=NULL,
-                     password=NULL,
+lsDownSearch<-function(satellite,
+                     startDate,
+                     endDate,
+                     username,
+                     password,
                      AppRoot,
                      lvl=1,
                      product=c("sr","source_metadata"),
                      verbose=FALSE,
+                     untar=TRUE,
                      raw.rm=FALSE,
-                     untar=FALSE,
-                     overwrite=FALSE,
-                     nattempts=5,
                      ...){
-  stopifnot(class(searchres)=="data.frame")
-  if(is.null(username)|is.null(password)){
-    stop("Username and/or password not defined!")
-  }
-  arg<-list(...)
-  AppRoot<-pathWinLx(AppRoot)
-  #identify mission
-  if(grepl("LC8",searchres[1,]$sceneID)){
-    downDir<-getRGISToolsOpt("LS8DownloadDir")
-  }else if(grepl("LE7",searchres[1,]$sceneID)){
-    downDir<-getRGISToolsOpt("LS7DownloadDir")
+  if(tolower(satellite)=="ls7"){
+    message("Searching Landsat-7 image time series.")
+    searchres=ls7Search(startDate=startDate,
+                        endDate=endDate,
+                        verbose=verbose,
+                        AppRoot=AppRoot,
+                        ...)
+  }else if (tolower(satellite)=="ls8"){
+    message("Searching Landsat-8 image time series.")
+    searchres=ls8Search(startDate=startDate,
+                        endDate=endDate,
+                        verbose=verbose,
+                        AppRoot=AppRoot,
+                        ...)
   }else{
-    stop("Unknown mission.")
+    stop("Satellite not supported. Perform the search with the argument satellite as 'ls7' or 'ls8'.")
   }
-  downPath<-file.path(AppRoot,downDir,"raw")
-  #create download folder
-  if(!file.exists(downPath)){
-    dir.create(downPath,recursive=TRUE)
+  if(verbose){
+    message("Search result:")
+    message(searchres)
   }
-  message(paste0("Downloading the images in: ",downPath))
-  if(lvl==1){
-    message("Starting Landsat level 1 download process...")
-    lsEarthExplorerdownload(searchres=searchres,
-                            username=username,
-                            password=password,
-                            cookies.file=NULL,
-                            downDir=downDir,
-                            downPath=downPath,
-                            AppRoot=AppRoot,
-                            verbose=verbose,
-                            untar=untar,
-                            raw.rm=raw.rm,
-                            overwrite=overwrite)
-  }else if(lvl==2){
-    message("Starting Landsat level 2 download process...")
-    lsEspaOrderImages(search.res=searchres,
-                      username=username,
-                      password=password,
-                      product=product,
-                      verbose=verbose)
-    message("Ordering images on ESPA platform...")
-    c.handle<-lsEspaCreateConnection(username=username,password=password)
-    images.order<-lsEspaGetOrderImages(c.handle=c.handle)
-    message("Cheking the order status and starting the download process.")
-    lsEspaDownloadOrders(orders=images.order,
-                         c.handle=c.handle,
-                         verbose=verbose,
-                         nattempts=nattempts,
-                         untar=untar,
-                         overwrite=overwrite,
-                         AppRoot=downPath)
-  }else{
-    stop("Landsat level not identified, check 'lvl'. Valid values 1 or 2.")
-  }
-  if(untar){
-    untarDir<-file.path(AppRoot,downDir,"untar")
-    message(paste0("The images have been downloaded and saved on HDD. \nFile path: ",untarDir))
-  }else{
-    message(paste0("The images have been downloaded and saved on HDD. \nFile path: ",downPath))
-  }
+  lsDownload(searchres=searchres,
+             username=username,
+             password=password,
+             untar=untar,
+             raw.rm=raw.rm,
+             AppRoot=AppRoot,
+             lvl=lvl,
+             product=product,
+             ...)
 }
-
-
-
-
